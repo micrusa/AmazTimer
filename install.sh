af73f23d0f5c5be0e6b1e19d9aef7f36fbfa2cb2
micrusa <micrusa@hotmail.es>
2020-04-11 16:21:48 +0200

    Remove uninstall from install script
    Signed-off-by: micrusa <micrusa@hotmail.es>

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bien/bash
 adb wait-for-device
 echo "Device detected!"
-adb shell "pm uninstall me.micrusa.amaztimer"
 adb install ./app/release/app-release.apk
 adb shell "am force-stop com.huami.watch.launcher"
