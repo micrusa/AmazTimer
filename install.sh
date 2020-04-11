365fc361e192f7d1eba41476b81604f0f8464532
micrusa <micrusa@hotmail.es>
2020-04-11 16:18:39 +0200

    Make app ready for signing
    Signed-off-by: micrusa <micrusa@hotmail.es>

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 adb wait-for-device
 echo "Device detected!"
 adb shell "pm uninstall me.micrusa.amaztimer"
-adb install ./app/build/outputs/apk/debug/app-debug.apk
+adb install ./app/release/app-release.apk
 adb shell "am force-stop com.huami.watch.launcher"
