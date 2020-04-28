1acb7879de02b12c2551cc3d0125e349a5ac9433
micrusa <micrusa@hotmail.es>
2020-04-28 20:15:51 +0200

    Removed unused items
    Signed-off-by: micrusa <micrusa@hotmail.es>

--- app/src/main/java/me/micrusa/amaztimer/activities/PresetsActivity.java
+++ app/src/main/java/me/micrusa/amaztimer/activities/PresetsActivity.java
@@ -23,7 +23,6 @@ public class PresetsActivity extends AppCompatActivity {
 
     private TextView preset1, preset2;
     private Button start1, start2, edit1, edit2;
-    private int sets1, sets2, work1, work2, rest1, rest2;
     private String textFormat = "SETS" + ": %s\n"
             + "WORK" + ": %t "
             + "REST" + ": %r";
