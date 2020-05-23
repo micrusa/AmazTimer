a8e1ac07acbc4af034bbe73db74664304f256d2d
micrusa <micrusa@hotmail.es>
2020-05-23 12:24:57 +0200

    Moved string to strings.xml
    Signed-off-by: micrusa <micrusa@hotmail.es>

--- app/src/main/java/me/micrusa/amaztimer/activities/AppInfo.java
+++ app/src/main/java/me/micrusa/amaztimer/activities/AppInfo.java
@@ -42,6 +42,6 @@ public class AppInfo extends AppCompatActivity {
         thanksto.setText(getResources().getString(R.string.thanksto)
                 + NEWLINE + "@Quinny899" + SPACE + getResources().getString(R.string.thanksfor) + SPACE + "Springboard Plugin Example"
                 + NEWLINE + "@GreatApo" + SPACE + getResources().getString(R.string.thanksfor) + SPACE + "Widget Calendar"
-                + NEWLINE + "All contributors");
+                + NEWLINE + getResources().getString(R.string.allcontributors));
     }
 }
