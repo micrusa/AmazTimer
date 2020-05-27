c4d85a5b32384f4eec037a15c2a39ccc13d94cdb
Miguel <micrusa@hotmail.es>
2020-05-27 12:53:07 +0200

    Setup huami UserInfo

--- app/src/main/java/me/micrusa/amaztimer/utils/UserInfo.java
+++ app/src/main/java/me/micrusa/amaztimer/utils/UserInfo.java
@@ -7,47 +7,60 @@ import android.icu.util.Calendar;
 import android.net.Uri;
 import android.util.Log;
 
+import me.micrusa.amaztimer.defValues;
+
 public class UserInfo {
+
+    private me.micrusa.amaztimer.defValues defValues = new defValues();
+    private Context context;
+
     int mGender;
-
     int mHeight;
-
     int mMonth;
-
     float mWeight;
-
     int mYear;
 
-    public static UserInfo getUserInfo(Context paramContext) {
+    public UserInfo(Context paramContext) {
+        context = paramContext;
         ContentResolver contentResolver = paramContext.getContentResolver();
         Cursor cursor = null;
-        paramContext = null;
         try {
-            Cursor cursor1 = contentResolver.query(Uri.parse("content://com.huami.watch.setup.usersettings"), null, null, null, null);
-            null = cursor1;
-            cursor = cursor1;
-            return new UserInfo(cursor1);
+            cursor = contentResolver.query(Uri.parse("content://com.huami.watch.setup.usersettings"), null, null, null, null);
         } catch (Exception exception) {
-            return null;
-        } finally {
-            if (exception != null)
-                exception.close();
+            Log.e("AmazTimer: UserInfo", "Exception while getting UserInfo: " + exception.getMessage());
+        }
+        if (cursor != null && cursor.moveToFirst()) {
+            this.mGender = cursor.getInt(cursor.getColumnIndex("gender"));
+            this.mYear = cursor.getInt(cursor.getColumnIndex("year"));
+            this.mMonth = cursor.getInt(cursor.getColumnIndex("month"));
+            this.mWeight = cursor.getFloat(cursor.getColumnIndex("weight"));
+            this.mHeight = cursor.getInt(cursor.getColumnIndex("height"));
         }
     }
 
-    public UserInfo(Cursor paramCursor) {
-        if (paramCursor != null && paramCursor.moveToFirst()) {
-            this.mGender = paramCursor.getInt(paramCursor.getColumnIndex("gender"));
-            this.mYear = paramCursor.getInt(paramCursor.getColumnIndex("year"));
-            this.mMonth = paramCursor.getInt(paramCursor.getColumnIndex("month"));
-            this.mWeight = paramCursor.getFloat(paramCursor.getColumnIndex("weight"));
-            this.mHeight = paramCursor.getInt(paramCursor.getColumnIndex("height"));
-        }
+    public int getAge() {
+        Log.d("HeartView", "mYear : " + this.mYear + "  defValues.CURRENT_YEAR :" + defValues.CURRENT_YEAR);
+        return defValues.CURRENT_YEAR - this.mYear;
     }
 
-    public int getAage() {
-        Log.d("HeartView", "mYear : " + this.mYear + "  Calendar.getInstance().get(Calendar.YEAR) :" + Calendar.getInstance().get(1));
-        return Calendar.getInstance().get(1) - this.mYear;
+    public int getGender(){
+        return this.mGender;
+    }
+
+    public float getWeight(){
+        return this.mWeight;
+    }
+
+    public int getHeight(){
+        return this.mHeight;
+    }
+
+    public void logEverything(){
+        Log.d("AmazTimer", "logEverything: GENDER: " + this.mGender
+                + " YEAR: " + this.mYear
+                + " MONTH: " + this.mMonth
+                + " WEIGHT: " + this.mWeight
+                + " HEIGHT: " + this.mHeight);
     }
 }
 
