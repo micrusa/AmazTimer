29c6f12c8961678f389273e518d00d3a7c79f27c
Miguel <micrusa@hotmail.es>
2020-06-25 20:49:21 +0200

    Replace hr array with an ArrayList
    Signed-off-by: Miguel <micrusa@hotmail.es>

--- app/src/main/java/me/micrusa/amaztimer/utils/latestTraining.java
+++ app/src/main/java/me/micrusa/amaztimer/utils/latestTraining.java
@@ -2,44 +2,32 @@ package me.micrusa.amaztimer.utils;
 
 import android.content.Context;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 import me.micrusa.amaztimer.defValues;
 
 public class latestTraining {
 
-    private int[] hrArray = null;
+    private ArrayList<Integer> hrArray = new ArrayList<>();
 
     void addHrValue(int value){
-        //If hrArray is null create it to avoid FC
-        if(this.hrArray == null){
-            this.hrArray = new int[0];
-        }
-        //Copy array to a bigger one
-        int[] newArray = new int[this.hrArray.length + 1];
-        System.arraycopy(this.hrArray, 0, newArray, 0, this.hrArray.length);
-        this.hrArray = newArray;
-        //Add new hr value to the array
-        this.hrArray[newArray.length - 1] = value;
+        this.hrArray.add(value);
     }
 
     void saveDataToFile(Context context, int time){
         file dataFile = new file(defValues.LATEST_TRAIN_FILE, context);
         file bodyFile = new file(defValues.BODY_FILE, context);
-        //Return if hrArray is null
-        if(this.hrArray == null){
-            return;
-        }
         //Get min and max heart rate
-        Arrays.sort(this.hrArray);
-        int minHr = this.hrArray[0];
-        int maxHr = this.hrArray[this.hrArray.length - 1];
+        int minHr = this.hrArray.indexOf(Collections.min(hrArray));
+        int maxHr = this.hrArray.indexOf(Collections.max(hrArray));
         //Calculate average heart rate
         int totalHr = 0;
         for (int value : this.hrArray) {
             totalHr = totalHr + value;
         }
-        int avgHr = totalHr / this.hrArray.length;
+        int avgHr = totalHr / this.hrArray.size();
         //Get body data from file
         int age = bodyFile.get(defValues.SETTINGS_AGE, defValues.DEFAULT_AGE);
         int weight = bodyFile.get(defValues.SETTINGS_WEIGHT, defValues.DEFAULT_WEIGHT);
@@ -48,8 +36,8 @@ public class latestTraining {
         int kcal = calculateKcal(avgHr, time, age, weight, isMale);
         //Save everything to file
         saveToFile(dataFile, minHr, maxHr, avgHr, kcal);
-        //Clean hrArray to avoid merging them when there is another training
-        this.hrArray = null;
+        //Clear hrArray to avoid merging them when there is another training
+        this.hrArray.clear();
     }
 
     public static int calculateKcal(int avgHr, int time, int age, int weight, boolean isMale){
@@ -66,8 +54,8 @@ public class latestTraining {
     }
 
     void cleanAllValues(Context context){
-        //Clean values and save default values to file, this way it wont conflict with other trainings
-        this.hrArray = null;
+        //Clear values and save default values to file, this way it wont conflict with other trainings
+        this.hrArray.clear();
         file dataFile = new file(defValues.LATEST_TRAIN_FILE, context);
         saveToFile(dataFile, defValues.DEFAULT_HR_VALUES, defValues.DEFAULT_HR_VALUES, defValues.DEFAULT_HR_VALUES, defValues.DEFAULT_KCAL_VALUES);
     }
