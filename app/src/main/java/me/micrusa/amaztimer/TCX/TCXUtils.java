3771f0a44825f74625305ca34dc888f7cd926891
Miguel <micrusa@hotmail.es>
2020-07-15 22:36:11 +0200

    Add a tcx time workaround in experimental settings

--- app/src/main/java/me/micrusa/amaztimer/TCX/TCXUtils.java
+++ app/src/main/java/me/micrusa/amaztimer/TCX/TCXUtils.java
@@ -1,12 +1,24 @@
 package me.micrusa.amaztimer.TCX;
 
+import com.pixplicity.easyprefs.library.Prefs;
+
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
+import me.micrusa.amaztimer.defValues;
+
 public class TCXUtils {
     public static String formatDate(Date date){
+        //Workaround for wrong times
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int hours = calendar.get(Calendar.HOUR) + Prefs.getInt(defValues.KEY_TCX_TIME, 0);
+        calendar.set(Calendar.HOUR, hours);
+        date = calendar.getTime();
+
         SimpleDateFormat dateFormat = new SimpleDateFormat(Constants.DATE_FORMAT, Locale.US);
         SimpleDateFormat timeFormat = new SimpleDateFormat(Constants.TIME_FORMAT, Locale.US);
         dateFormat.setTimeZone(TimeZone.getDefault());
