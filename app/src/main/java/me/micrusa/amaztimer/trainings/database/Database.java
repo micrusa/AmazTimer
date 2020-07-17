d4591c0cdc1702de0c93a92fc1e32d8cf9ffb028
Miguel <micrusa@hotmail.es>
2020-07-17 14:58:18 +0200

    Fix build

--- app/src/main/java/me/micrusa/amaztimer/trainings/database/Database.java
+++ app/src/main/java/me/micrusa/amaztimer/trainings/database/Database.java
@@ -4,15 +4,42 @@ import com.dbflow5.config.DBFlowDatabase;
 import com.dbflow5.config.FlowManager;
 import com.dbflow5.database.DatabaseWrapper;
 
+import org.jetbrains.annotations.NotNull;
+
 @com.dbflow5.annotation.Database(name = Database.NAME, version = Database.VERSION)
-public class Database {
+public class Database extends DBFlowDatabase {
 
     public static final String NAME = "AmazTimer";
 
     public static final int VERSION = 1;
 
     public static DatabaseWrapper getDb(){
-        return FlowManager.getDatabase(NAME);
+        return FlowManager.getDatabase(Database.class);
     }
 
+    @NotNull
+    @Override
+    public Class<?> getAssociatedDatabaseClassFile() {
+        return null;
+    }
+
+    @Override
+    public int getDatabaseVersion() {
+        return VERSION;
+    }
+
+    @Override
+    public boolean isForeignKeysSupported() {
+        return false;
+    }
+
+    @Override
+    public boolean areConsistencyChecksEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean backupEnabled() {
+        return false;
+    }
 }
