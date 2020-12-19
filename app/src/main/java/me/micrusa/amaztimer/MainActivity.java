c82e127d839f8f5d4d0e7c14064e7dfbfc9900f2
Miguel <micrusa@hotmail.es>
2020-12-19 12:25:51 +0100

    Implement basic MainActivity with new design & add CreateNew and Saved activities

--- app/src/main/java/me/micrusa/amaztimer/MainActivity.java
+++ app/src/main/java/me/micrusa/amaztimer/MainActivity.java
@@ -26,18 +26,29 @@ package me.micrusa.amaztimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Button;
+
+import me.micrusa.amaztimer.activities.CreateNew;
+import me.micrusa.amaztimer.activities.Saved;
+import me.micrusa.amaztimer.activities.SettingsActivity;
+import me.micrusa.amaztimer.utils.Utils;
+import me.micrusa.amaztimer.utils.button.buttonEvent;
+import me.micrusa.amaztimer.utils.button.buttonListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button saved, createNew, settings;
+    private boolean hasLaunchedActivities = false;
+    private buttonListener buttonListener = new buttonListener();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setupBtnListener();
         init();
     }
 
@@ -45,5 +56,45 @@ public class MainActivity extends AppCompatActivity {
         saved = findViewById(R.id.main_saved);
         createNew = findViewById(R.id.main_create_new);
         settings = findViewById(R.id.main_settings);
+
+        saved.setOnClickListener(view -> launchActivity(new Intent(this, Saved.class)));
+        createNew.setOnClickListener(view -> launchActivity(new Intent(this, CreateNew.class)));
+        settings.setOnClickListener(view -> launchActivity(new Intent(this, SettingsActivity.class)));
+    }
+
+    private void setupBtnListener(){
+        Handler handler = new Handler();
+        buttonListener.start(this, ButtonEvent -> {
+            if(ButtonEvent.getKey() == buttonEvent.KEY_UP)
+                handler.post(() -> saved.performClick());
+            else if(ButtonEvent.getKey() == buttonEvent.KEY_CENTER)
+                handler.post(() -> createNew.performLongClick());
+            else if(ButtonEvent.getKey() == buttonEvent.KEY_DOWN)
+                handler.post(() -> settings.performLongClick());
+        });
+    }
+
+    private void launchActivity(Intent intent){
+        if(hasLaunchedActivities) return; //Avoid multiple activities launched
+        Utils.vibrate(Constants.HAPTIC_VIBRATION, this);
+        hasLaunchedActivities = true;
+        buttonListener.stop();
+        startActivity(intent);
+    }
+
+    public void onStop() {
+        super.onStop();
+        buttonListener.stop();
+    }
+
+    public void onPause() {
+        buttonListener.stop();
+        super.onPause();
+    }
+
+    public void onResume() {
+        hasLaunchedActivities = false;
+        setupBtnListener();
+        super.onResume();
     }
 }
